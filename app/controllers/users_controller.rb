d3a36500bd6b0995e38f92fbcb3f361dcd6d9029
Grace Tan <gracechengtan@gmail.com>
2014-11-23 19:18:08 -0500

    add edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,8 +17,21 @@ class UsersController < ApplicationController
       render 'new'
     end    
   end
-  private
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+     @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
